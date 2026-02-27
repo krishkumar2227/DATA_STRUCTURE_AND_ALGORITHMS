0a8c3bd3a6f960c668db838808831c8b6c42152a
krishkumar2227 <krishkumar.200622@gmail.com>
2026-02-27 20:55:39 +0530

    LEETCODE SOLUTIONS

---
+++ Arrays/Median_of_two_SortedArray.py
@@ -0,0 +1,36 @@
+class solution(object):
+    def findMedianSortedArrays(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: float
+        """
+
+        result = []
+        i=0
+        j=0
+       
+        while i < len(nums1) and j < len(nums2):
+            if nums1[i]<nums2[j]:
+                result.append(nums1[i])
+                i=i+1
+            else:
+                result.append(nums2[j])
+                j=j+1
+
+        while i < len(nums1):
+            result.append(nums1[i])
+            i=i+1
+        while j < len(nums2):
+            result.append(nums2[j])
+            j=j+1
+
+        if len(result)%2==0:
+          index1=len(result)//2-1
+          index2=len(result)//2
+          return (result[index1]+result[index2])/2.0
+        else:
+            return result[len(result)//2]   
+       
+    
+     
