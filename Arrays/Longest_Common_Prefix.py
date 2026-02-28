7181344971c65edd2a0db67ab17c4f5bcc081871
krishkumar2227 <krishkumar.200622@gmail.com>
2026-02-28 15:11:19 +0530

    LEETCODE SOLUTIONS

---
+++ Arrays/Longest_Common_Prefix.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        if len(strs)==0:
+            return ""
+        
+        prefix=""
+        for i in range(len(strs[0])):
+            current_char=strs[0][i]
+            for word in strs:
+                if i >=len(word)  or word[i]!=current_char:
+                    return prefix
+            prefix+=current_char
+        return prefix
