899e04c61afe3f72c65a54cd9ebb4e4c9e428d11
krishkumar2227 <krishkumar.200622@gmail.com>
2026-03-18 20:02:48 +0530

    LEETCODE SOLUTIONS

--- Arrays/Merge_Sorted_Array.py
+++ Arrays/Merge_Sorted_Array.py
@@ -15,13 +15,13 @@ class Solution(object):
                 nums1[k]=nums1[i]
                 i-=1
             else:
-                nums1[k]=nums2[j];
+                nums1[k]=nums2[j]
                 j-=1
             
             k-=1
         
         while(j>=0):
-            nums1[k]=nums2[j];
+            nums1[k]=nums2[j]
             j-=1
             k-=1
         
