74771e1971bb307ae5838a224545fc00487800b2
krishkumar2227 <krishkumar.200622@gmail.com>
2026-03-21 20:43:59 +0530

    BASIC qUESTIONS

---
+++ 01_Foundations_Questions/Sum.py
@@ -0,0 +1,10 @@
+def my_sum(*args):
+  total=0
+  for num in args:
+    total+=num
+  return total
+
+print(my_sum(1, 2, 3))        
+print(my_sum(10))            
+print(my_sum())              
+print(my_sum(5, -5, 10, 20))
